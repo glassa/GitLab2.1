03e76464ec57bd076121a6305ef9e9da322fa027
imdokdokhe <tonykslee1@gmail.com>
2016-10-06 12:16:24 -0700

    commit random stuff

---
+++ app/src/main/java/tcss450/uw/edu/gitlab2/TonyUpdate.java
@@ -0,0 +1,18 @@
+package tcss450.uw.edu.gitlab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TonyUpdate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tony_update);
+    }
+
+    //random method to commit
+    public void doSomething() {
+        
+    }
+}
